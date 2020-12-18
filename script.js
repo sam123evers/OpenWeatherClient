037f1982a06e324f8c28abe637105432e2d79e07
Sam Evers <sam123evers@gmail.com>
2020-12-18 16:54:18 -0600

    adds working ajax call to open weather api

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
+var apikey = '79a00d357f87a1ac91275e55183d49d2';
+var apiurl = 'http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=';
+
 // make ajax call to open weather api
-console.log($);
+$.ajax({
+    url: apiurl + apikey,
+    success: function(result, status, xhr){
+        console.log(result);
+        console.log(status);
+        console.log(xhr)
+    }
+});
 
 // consume / map data how you want it
 
