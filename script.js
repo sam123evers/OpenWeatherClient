9037f2dade3151cf7e881f1a5e885054db9f9332
Sam Evers <sam123evers@gmail.com>
2020-12-19 12:53:52 -0600

    adds rendered daily and hourly weather data

--- script.js
+++ script.js
@@ -3,6 +3,11 @@ var apiUrlBase = 'https://api.openweathermap.org/data/2.5/onecall';
 var urlWithParams;
 var apiResponse;
 
+var iconBaseUrl = "http://openweathermap.org/img/wn/";
+// replace 10d with icon string and desired size after @;
+// eg: "http://openweathermap.org/img/wn/10d@2x.png";
+
+
 
 // define function that build the URL
 function assembleURL(base, lat, long, key){
@@ -24,15 +29,14 @@ function onPositionFound(positionObj) {
     var lat = positionObj.coords.latitude;
     var long = positionObj.coords.longitude;
 
-    // we assume to have found the position here,
-    // so we're not checking for it
-
-
     // make ajax call to open weather api
     $.ajax({
         url: assembleURL(apiUrlBase, lat, long, apikey),
         success: function(result){
             handleApiResponseData(result);
+        },
+        error: function(err) {
+            alert(err);
         }
     });
 };
@@ -43,18 +47,17 @@ function onPositionNotFound() {
 
 
 function handleApiResponseData(weatherData) {
-    var current = weatherData.current;
     var minutely = weatherData.minutely;
-    var hourlyWeatherData = weatherData.hourly;
-    var daily = weatherData.daily;
 
-    handleHourlyWeatherData(hourlyWeatherData);
+    handleCurrentWeatherData(weatherData.current); 
+    handleHourlyWeatherData(weatherData.hourly);
+    handleDailyWeatherData(weatherData.daily);
 }   
 
-function handleHourlyWeatherData(hoursData) {
+function handleHourlyWeatherData(hrsDataObj) {
     var labels = [];
     var dataSet = [];
-    hoursData.forEach((dp, i) => {
+    hrsDataObj.forEach((dp, i) => {
         //console.log(dp);
         if(i % 3 === 0 && i < 24) {
             var readableTime = moment.unix(dp.dt).format('h:mm A'); 
@@ -64,7 +67,7 @@ function handleHourlyWeatherData(hoursData) {
     });
 
 
-    var ctx = document.getElementById('hourlyWeatherCanvas').getContext('2d');
+    var ctx = document.getElementById('hourlyCanvas').getContext('2d');
 
 	var chart = new Chart(ctx, {
 		// The type of chart we want to create
@@ -81,9 +84,63 @@ function handleHourlyWeatherData(hoursData) {
 			}]
 		},
 
-		// Configuration options go here
+		// Hourly Chart Config below
 		options: {}
 	});
+}
+
+function handleDailyWeatherData(dlyWeatherObj) {
+    console.log("I'm daily!", dlyWeatherObj);
+
+    dlyWeatherObj.map((dp) => {
+        // grab container from DOM; store in variable
+        var main = document.getElementById("dlyWeatherContainer");
+
+        // create four divs
+        var outer = document.createElement("div");
+        outer.className = "daily-weather-cell";
+        var dayContainer = document.createElement("div");
+        var iconContainer = document.createElement("div");
+        var tempContainer = document.createElement("div");
+
+        // get unix time and display as DOW
+        var dayOfWeek = moment.unix(dp.dt).format('dddd'); 
+
+        // max and min temps
+        // create p tags; put data inside
+        var low = document.createElement("p");
+        var high = document.createElement("p");
+        high.innerText = dp.temp.max;
+        low.innerText = dp.temp.min;
+        tempContainer.append(high, low);
+
+        // create image tag and assign its source to icon URL
+        var iconImage = document.createElement("img");
+        iconImage.src = iconBaseUrl + dp.weather[0].icon + ".png";
+
+        // create p tag; put DOW inside
+        var dowP = document.createElement("p");
+        dowP.innerText = dayOfWeek;
+
+
+        
+        outer.append(dowP);
+        outer.append(iconImage);
+        outer.append(tempContainer);
+
+        
+
+        //console.log(dp.temp.max);
+        //console.log(dp.temp.min);
+        console.log(dp.weather[0].icon);
+        console.log(dp.weather[0].description);
+
+        main.append(outer);
+    });
+};
+
+function handleCurrentWeatherData(crtWeatherObj) {
+    console.log("I'm Current!", crtWeatherObj);
 }
 
 navigator.geolocation.getCurrentPosition(onPositionFound, onPositionNotFound);
